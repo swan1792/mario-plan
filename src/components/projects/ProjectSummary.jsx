1cc945dc1deb04017decfb5b20ed57a5d7264854
swan1792 <swamhtetn31@gmail.com>
2025-11-07 14:50:52 +0630

    Lesson-12

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const ProjectSummary = () => {
+const ProjectSummary = ({project}) => {
   return (
     <div className="card z-depth-0 project-summary s12">
       <div className="card-content">
-        <span className="card-title">Project Title</span>
-        <p>Posted by Dev Team</p>
+        <span className="card-title">{project.title}</span>
+        <p>{project.content}</p>
         <p className="grey-text">3rd September</p>
       </div>
     </div>
