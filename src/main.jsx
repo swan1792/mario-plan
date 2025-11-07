4a97210a431574c2f4415eb5b037cde811a2afc9
swan1792 <swamhtetn31@gmail.com>
2025-11-07 13:58:52 +0630

    lesson-11

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import rootReducer from './store/reducers/rootReducer.js';
+import { Provider } from 'react-redux';
+import { createStore} from 'redux';
 
+const store = createStore(rootReducer);
 createRoot(document.getElementById('root')).render(
-    <App />
+    <Provider store={store}>
+       <App />
+    </Provider>
 )
